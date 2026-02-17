c92a727e264a4e4c06f04967573fe7213f6a6e79
Mathew Prokuda <mlp1769@g.rit.edu>
2026-02-17 17:15:50 -0500

    Wrote todo strings to plan out what I need to do when I create the Main and BufferManager classes.

--- AppLogic/src/base/buffer/BufferManager.java
+++ AppLogic/src/base/buffer/BufferManager.java
@@ -1,6 +1,40 @@
 package base.buffer;
+import base.models.Page;
 
+import java.util.HashMap;
 
 
 public class BufferManager {
+
+    private const int maxPageCount = 1000;
+    private static HashMap<Page> buffer;
+
+    //todo make static instance of the BufferManager similar to how the bufferManager is done
+    private static BufferManager bufferManager = null;
+
+
+
+    //todo buffer is inside of the buffermanage (stored as Hashmap of Pages)
+
+    //todo to get least recently used page, get the values and find the oldest timestamp
+
+    //todo createNewPage() -> create new Page object and store it in the hashmap
+
+    //todo if createNewPage() is called and the # of entries in the hash, eject the least recently used page to hardware.
+    //todo call storage manager to save the page
+
+
+    public Page getPage(id){
+
+    }
+
+    public Page createNewPage(...){
+
+    }
+
+    private Page readPagefromHardware(id){
+
+    }
+
+    private void writePageToHardware(Page){}
 }
--- AppLogic/src/base/models/Page.java
+++ AppLogic/src/base/models/Page.java
@@ -1,4 +1,6 @@
 package base.models;
 
 public class Page {
+
+    //todo the Page is just a list of pointers to records
 }
