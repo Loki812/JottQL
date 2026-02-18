c0548691823b0777532108cd26480e1a369da4a0
Mathew Prokuda <mlp1769@g.rit.edu>
2026-02-18 18:38:16 -0500

    In the AttrubuteValue class, I added data-members, a constructor, a compareTo() function, and a toString() function.

--- AppLogic/src/base/models/AttributeValue.java
+++ AppLogic/src/base/models/AttributeValue.java
@@ -1,2 +1,21 @@
-package base.models;public class AttributeValue {
+package base.models;
+
+public class AttributeValue<T> {
+    public T data;
+    public DataTypes type;
+
+    public AttributeValue(T data, DataTypes type){
+        this.data = data;
+        this.type = type;
+    }
+
+
+    public int compareTo(AttributeValue<T> attribute){
+        return this.toString().compareTo(attribute.toString());
+    }
+
+    @Override
+    public String toString() {
+        return data.toString();
+    }
 }
