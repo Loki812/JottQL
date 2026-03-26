757d7c8ad09e6b34c2166cca74cced5617900ad5
Mathew Prokuda <mlp1769@g.rit.edu>
2026-03-25 20:53:56 -0400

    Stubbed out and wrote some sudo-code for buildWhereTree() and where() functions in Where class.

--- AppLogic/src/base/parse/DML/Where.java
+++ AppLogic/src/base/parse/DML/Where.java
@@ -1,2 +1,30 @@
-package base.parse.DML;public class Where {
+package base.parse.DML;
+
+import base.models.Page;
+import base.models.whereNodes.WhereTreeNode;
+
+import java.util.ArrayList;
+
+public class Where {
+
+    public WhereTreeNode buildWhereTree(String wherePieces){
+        //todo return root of where tree
+    }
+
+    public ArrayList<Record> where(Page page, String tableName, String wherePieces){
+        //todo call buildWhereTree
+
+        /*
+        for (i in page's list of record){
+            check if it should be in the new table from where-condition
+
+            if( page's nextTableID != -1){
+                do the loop again on the next page
+            }
+
+        }
+         */
+
+        //todo for temporary tables, call TableSchema's copy() function
+    }
 }
