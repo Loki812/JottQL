63b843735f1a63e230c1176f69d4d27a2fa9d7e4
Defenestrated02 <om.malkan@gmail.com>
2026-04-15 14:36:09 -0400

    removed unnesseary line

--- AppLogic/src/base/parse/DML/Cartesian.java
+++ AppLogic/src/base/parse/DML/Cartesian.java
@@ -64,10 +64,7 @@ public class Cartesian {
             }
             table1page = buffer.getPage(table1page.nextPageId);
         }
-        //if doing multiple joins clean up extra tables
-        if(table1.startsWith("_TEMP_")){
-            TableSchema.addTemp(table1);
-        }
+
         //recurse
         tableNames.addFirst(newTable);
         return Product(tableNames);
